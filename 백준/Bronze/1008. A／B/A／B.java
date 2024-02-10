07c428bc159b8e29b1474a1d9a7de24f405873e1
kssim1101 <153337243+kssim1101@users.noreply.github.com>
2024-02-10 17:06:47 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    double A = scanner.nextDouble();
+    double B = scanner.nextDouble();
+    System.out.println(A/B);
+}
+}
