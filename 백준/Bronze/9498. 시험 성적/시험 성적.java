cc8555522767e4e677981b7fb9289d4f3ac99a71
kssim1101 <153337243+kssim1101@users.noreply.github.com>
2024-02-11 17:54:27 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if(a<=100 && a>=90){
+            System.out.println("A");
+        }else if(a<=89 && a>=80){
+            System.out.println("B");
+        }else if(a<=79 && a>=70){
+            System.out.println("C");
+        }else if(a<=69 && a>=60){
+            System.out.println("D");
+        }else{
+            System.out.println("F");
+        }
+    }
+}
