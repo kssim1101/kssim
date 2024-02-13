bc154ab1be4ce6afbe95ff563c805030850fad4b
kssim1101 <153337243+kssim1101@users.noreply.github.com>
2024-02-13 13:55:13 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18664 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for(int i = 1; i<=t; i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Case #"+i+": "+ (a+b));
+        }
+    }
+}
