9bdffd3b0e257aaacf42e136da0f2025999c90e3
kssim1101 <153337243+kssim1101@users.noreply.github.com>
2024-02-15 17:10:01 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 656 ms, Memory: 36196 KB -BaekjoonHub

---
+++ 백준/Bronze V/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Main{    
+    public static void main(String[] args){
+ 
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int a[][] = new int[n][m];
+        int b[][] = new int[n][m];
+        int sum[][] = new int[n][m];
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                a[i][j] = scanner.nextInt();
+            }
+        }
+            for(int i = 0; i<n; i++) {
+                for(int j = 0; j<m; j++){
+                    b[i][j] = scanner.nextInt();
+                }
+            }
+        for(int i = 0; i<n; i++){
+                for(int j = 0; j<m; j++){
+                     sum[i][j] = a[i][j]+b[i][j];
+                }
+            }
+             for(int i = 0; i<n; i++) {
+                for(int j = 0; j<m; j++){
+                     System.out.print(sum[i][j]+" ");
+                }
+                 System.out.println();
+      }
+  }
+}
