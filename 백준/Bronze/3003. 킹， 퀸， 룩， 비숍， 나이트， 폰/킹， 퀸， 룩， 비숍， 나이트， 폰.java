56b85e5fd71bd705bdfc0463855b12510b815b47
kssim1101 <153337243+kssim1101@users.noreply.github.com>
2024-02-27 17:02:16 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 272 ms, Memory: 19264 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        
+        int aa = 1;
+        int bb = 1;
+        int cc = 2;
+        int dd = 2;
+        int ee = 2;
+        int ff = 8;
+        
+        int aaa = aa -a;
+        int bbb = bb -b;
+        int ccc = cc -c;
+        int ddd = dd -d;
+        int eee = ee -e;
+        int fff = ff -f;
+        System.out.println(aaa+" "+bbb+" "+ccc+" "+ddd+" "+eee+" "+fff);
+        
+    }
+}
