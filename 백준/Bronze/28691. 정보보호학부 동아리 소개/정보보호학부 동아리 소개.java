2b49d04b9ad9f5fee1e8784c54a64eca1b1e9737
kssim1101 <153337243+kssim1101@users.noreply.github.com>
2024-04-20 11:26:23 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 200 ms, Memory: 17464 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        if(str.equals("M")){
+            System.out.println("MatKor");
+        }else if(str.equals("W")){
+            System.out.println("WiCys");
+        }else if(str.equals("C")){
+            System.out.println("CyKor");
+        }else if(str.equals("A")){
+            System.out.println("AlKor");
+        }else{
+            System.out.println("$clear");
+        }
+    }
+}
